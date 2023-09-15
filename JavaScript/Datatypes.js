87221fa2fbb13c499d6e9ed4774fcdd1edde25f4
Somu Mane <106975847+somumane@users.noreply.github.com>
2023-09-15 07:27:54 +0000

    Datatypes

---
+++ JavaScript/Datatypes.js
@@ -0,0 +1,18 @@
+/* Data Types are two Types Premitive NonPremitive */
+
+// Premitive Data Types
+
+let string="Somu"  //String " ", '' 
+let number=82  //number range 2 power 53 also bigint is there for bigervalues
+let boolean=true //boolean true/false
+let Null=null // its refers empty value store
+let Undefine=undefined // value assigned not define
+
+// symbol is also there but its only using unique
+
+//Non Premitive
+
+const obj={} //Objects
+const arr=[] // Arrays
+
+console.table([typeof(string),typeof(number),typeof(boolean),typeof(Null),typeof(Undefine),typeof(obj),typeof(arr)])
